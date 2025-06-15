3befe47f77f7c7c76399f5c054a044b90aa06c61
amantur <amantur.mamatazimov04@gmail.com>
2025-06-15 10:58:28 +0600

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,12 @@ const ADMIN_USER = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(cors({
+  origin: [""],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 // Конфигурация сессий
 app.use(session({
